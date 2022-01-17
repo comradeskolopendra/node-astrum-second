587779cda8dc572a4ee124a7d70a55fcc98bb5a5
comradeskolopendra <91865240+comradeskolopendra@users.noreply.github.com>
2022-01-17 14:20:16 +0600

    Add files via upload

--- serviceLogger.js
+++ serviceLogger.js
@@ -1,16 +1,35 @@
 const fs = require('fs');
 const path = require('path')
-
 var EventLogger = require('node-windows').EventLogger;
-var logMain = new EventLogger('Astrum-Telegram')
-let logThread = new EventLogger('Astrum-Telegram-Thread')
+const sqlite3 = require('sqlite3')
+const sqlite = require('sqlite')
 
-setInterval(() => {
-  fs.readFile(path.join(__dirname, '/package.json'), (err, content) => {
+let logThread = new EventLogger('Astrum-Telegram')
+
+fs.readFile(path.join(__dirname, '/package.json'), (err, content) => {
+  setInterval(() => {
     if (err) {
-      logMain.warn(err)
+      logThread.warn(err)
     }
 
-    logMain.info(content.toString())
+    logThread.info('[Logger]')
+  }, 1000)
+})
+
+fs.readFile(path.join(__dirname, '/package.json'), (err, content) => {
+  if (err) {
+    logThread.warn(err)
+  }
+
+  setInterval(() => {
+    sqlite.open({
+      filename: 'main.db',
+      driver: sqlite3.Database
+    }).then((db) => {
+      db.get('SELECT number FROM users WHERE type = ?', '0').then((response) => {
+          logThread.info(response.number)
+      }
+    )
   })
-}, 500)
+  }, 20000)
+})
